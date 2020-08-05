35731b85f4c156aedb91ecf17a511cdd53393b32
huuthe1999 <55992625+huuthe1999@users.noreply.github.com>
2020-08-05 18:43:49 +0700

    Add record database

--- db/quanlythuvien_phieu_tra.sql
+++ db/quanlythuvien_phieu_tra.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+-- MariaDB dump 10.17  Distrib 10.4.13-MariaDB, for Win64 (AMD64)
 --
 -- Host: localhost    Database: quanlythuvien
 -- ------------------------------------------------------
--- Server version	8.0.20
+-- Server version	10.4.13-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,17 +21,17 @@
 
 DROP TABLE IF EXISTS `phieu_tra`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `phieu_tra` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_phieu_muon` int DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_phieu_muon` int(11) DEFAULT NULL,
   `ngay_tra` date DEFAULT NULL,
   `tinh_trang` varchar(45) DEFAULT NULL,
   `boi_thuong` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_id_phieu_muon_phieu_tra_idx` (`id_phieu_muon`),
   CONSTRAINT `fk_id_phieu_muon_phieu_tra` FOREIGN KEY (`id_phieu_muon`) REFERENCES `phieu_muon` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +40,7 @@ CREATE TABLE `phieu_tra` (
 
 LOCK TABLES `phieu_tra` WRITE;
 /*!40000 ALTER TABLE `phieu_tra` DISABLE KEYS */;
+INSERT INTO `phieu_tra` VALUES (1,5,'2020-09-09','Bình thường',NULL),(2,6,'2020-08-22','Bình thường',NULL),(3,7,'2020-10-01','Bình thường',NULL),(4,8,'2020-08-15','Bình thường',NULL),(5,9,'2020-09-28','Hư Hỏng','Có');
 /*!40000 ALTER TABLE `phieu_tra` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-05 11:02:46
+-- Dump completed on 2020-08-05 18:40:25
--- db/quanlythuvien_tac_gia.sql
+++ db/quanlythuvien_tac_gia.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+-- MariaDB dump 10.17  Distrib 10.4.13-MariaDB, for Win64 (AMD64)
 --
 -- Host: localhost    Database: quanlythuvien
 -- ------------------------------------------------------
--- Server version	8.0.20
+-- Server version	10.4.13-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,13 +21,13 @@
 
 DROP TABLE IF EXISTS `tac_gia`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tac_gia` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `ten_tac_gia` varchar(50) DEFAULT NULL,
   `mo_ta` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,6 +36,7 @@ CREATE TABLE `tac_gia` (
 
 LOCK TABLES `tac_gia` WRITE;
 /*!40000 ALTER TABLE `tac_gia` DISABLE KEYS */;
+INSERT INTO `tac_gia` VALUES (1,'Nguyễn Nhật Ánh',NULL),(2,'Nguyễn Phong Việt',NULL),(3,'Anh Khang',NULL),(4,'Nguyễn Ngọc Thạch',NULL),(5,'Nguyễn Ngọc Sơn',NULL);
 /*!40000 ALTER TABLE `tac_gia` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -48,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-05 11:02:46
+-- Dump completed on 2020-08-05 18:40:25
